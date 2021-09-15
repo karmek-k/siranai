f5a83ca77092c9d522bdcc70413374054eab1d6a
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-15 21:06:51 +0200

    Bind Logger to WinstonLogger

--- src/container/inversify.config.ts
+++ src/container/inversify.config.ts
@@ -1,10 +1,12 @@
 import { Container } from 'inversify';
-import { Database, DiscordClient } from './interfaces';
+import { Database, DiscordClient, Logger } from './interfaces';
 import types from './types';
 import { DefaultDiscordClient } from '../services/discord';
+import { WinstonLogger } from '../services/logger';
 
 const container = new Container();
 container.bind<Database>(types.database);
 container.bind<DiscordClient>(types.discordClient).to(DefaultDiscordClient);
+container.bind<Logger>(types.logger).to(WinstonLogger);
 
 export default container;
