728de853fa119984529551bc43f793957e1889f7
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-16 20:20:20 +0200

    Load programmatically all commands for registering

--- src/cli/registerCommands.ts
+++ src/cli/registerCommands.ts
@@ -1,17 +1,26 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
+import fs from 'fs';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 import { Logger } from '../container/interfaces';
 import container from '../container/inversify.config';
 import types from '../container/types';
+import path from 'path';
 
 const clientId = process.env.CLIENT_ID!;
 const guildId = process.env.GUILD_ID!;
 const token = process.env.BOT_TOKEN!;
 
-const commands = [
-  new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!')
-].map(command => command.toJSON());
+const commandFilesPath = path.join(__dirname, '..', 'commands');
+const commandFiles = fs
+  .readdirSync(commandFilesPath)
+  .filter(file => file.endsWith('.js'));
+
+const commands = commandFiles.map(file => {
+  const commandPath = path.join(commandFilesPath, file);
+  const command = require(commandPath).default;
+
+  return command.data.toJSON();
+});
 
 const rest = new REST({ version: '9' }).setToken(token);
 
