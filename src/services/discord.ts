d136ca247bf17e41b8e0fd6115cc760ed2d104e2
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-16 11:38:28 +0200

    Inject the command handler to the discord client

--- src/services/discord.ts
+++ src/services/discord.ts
@@ -1,14 +1,20 @@
 import { Client, Intents } from 'discord.js';
 import { inject, injectable } from 'inversify';
-import { DiscordClient, Logger } from '../container/interfaces';
+import { CommandHandler, DiscordClient, Logger } from '../container/interfaces';
 import types from '../container/types';
 
 @injectable()
 export class DefaultDiscordClient implements DiscordClient {
   private client: Client;
 
-  constructor(@inject(types.logger) private logger: Logger) {
+  constructor(
+    @inject(types.logger) private logger: Logger,
+    @inject(types.commandHandler) private handler: CommandHandler
+  ) {
     this.client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+    this.client.on('interactionCreate', handler.handle);
+
     this.client.once('ready', () => {
       this.logger.log('Bot is ready');
     });
